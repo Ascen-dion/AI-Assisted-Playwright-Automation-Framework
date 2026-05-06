ca1a0a4cf26d60831554a3d88fb330a82ade15c3
viplove <you@example.com>
2026-05-06 18:02:59 +0530

    delete files

--- src/web/locators/ud-about-us-nav.locators.js
+++
@@ -1,8 +0,0 @@
-// === FILE: src/pages/locators/ud-about-us-nav.locators.js ===
-const locators = {
-  aboutUsNavLink: (page) => page.getByText('About Us').first(),
-  heroHeading: (page) => page.getByRole('heading', { name: 'BANKING FOR THE BETTER' }),
-  valuesHeading: (page) => page.getByRole('heading', { name: 'OUR VALUES' }),
-};
-
-module.exports = locators;
